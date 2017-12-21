60e4e316278e3c0f61a628157a40b73798eb824f
Tbfaldhi <tubagusfaldhi@gmail.com>
2017-12-21 17:20:40 +0700

    benerin

--- exercise-13.js
+++ exercise-13.js
@@ -1,8 +1,8 @@
 var input = [
-                ['0001', 'Roman Alamsyah', 'Bandar Lampung', '21/05/1989', 'Membaca'],
+                ['0001', 'Roman Alamsyah', 'Bandar Lampung',' 21/05/1989', 'Membaca'],
                 ['0002', 'Dika Sembiring', 'Medan', '10/10/1992', 'Bermain Gitar'],
-                ['0003', 'Winona', 'Ambon', '25/12/1965', 'Memasak'],
-                ['0004', 'Bintang Senjaya', 'Martapura', '6/4/1970', 'Berkebun']
+                ['0003', 'Winona', 'Ambon','25/12/1965', 'Memasak'],
+                ['0004', 'Bintang Senjaya', 'Martapura',' 6/4/1970', 'Berkebun']
             ]
 
 var form = [
@@ -12,17 +12,15 @@ var form = [
             ['nomor id: ','Nama lengkap: ','TTL: ','hobi: ']
             ]
 
-for (var i=0, len=input.length; i<len; i++) {
-    // inner loop applies to sub-arrays
-    for (var j=0, len2=input[i].length; j<len2; j++) {
-        // accesses each element of each sub-array in turn
-         for (var i=0, len=form.length; i<len; i++) {
-    // inner loop applies to sub-arrays
-    for (var j=0, len2=form[i].length; j<len2; j++) {
-        // accesses each element of each sub-array in turn
-        console.log( form[i][j]+input[i][j])
-    }
+for (var i = 0; i<input.length; i++) {
+        for (var j=0; j<form[i].length; j++) {}
 }
 
-    }
+for (var i=0; i<input.length; i++) {
+    for (var j=0; j<form[i].length; j++) {}
+      console.log(  form[i][0]+input[i][0]);
+      console.log(  form[i][1]+input[i][1]);
+      console.log(  form[i][2] +input[i][2] +''+ input[i][3]);
+      console.log(  form[i][3]+input[i][4]);
+
 }
